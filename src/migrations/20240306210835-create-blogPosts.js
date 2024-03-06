42d31252e7df74c9c117ede776615ec1f9d36eab
andrew-marquezin <andrewmmarinho@gmail.com>
2024-03-06 18:30:32 -0300

    requisito 1 foi

--- src/migrations/20240306210835-create-blogPosts.js
+++ src/migrations/20240306210835-create-blogPosts.js
@@ -3,33 +3,42 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('users', {
+    await queryInterface.createTable('blog_posts', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
-      displayName: {
-        allowNull: false,
-        field: 'display_name',
-        type: Sequelize.STRING,
-      },
-      email: {
+      title: {
         allowNull: false,
         type: Sequelize.STRING,
       },
-      password: {
+      content: {
         allowNull: false,
         type: Sequelize.STRING,
       },
-      image: {
-        type: Sequelize.STRING,
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
       },
     });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('users');
+    await queryInterface.dropTable('blog_posts');
   }
 };
---
+++ src/migrations/20240306212118-create-postsCategories.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('posts_categories', {
+      postId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'post_id',
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+      },
+      categoryId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        field: 'category_id',
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('posts_categories');
+  }
+};
